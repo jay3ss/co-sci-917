09039bdd17d01b4ce2eb5a959014e09ea722adf0
Jay Ess <mentoce@gmail.com>
2019-04-01 19:12:23 -0700

    Have the basics of the program going. Just need to print better. Problem: still not y-centered

--- assignments/assignment04/SimpleAddition1.asm
+++ assignments/assignment04/SimpleAddition1.asm
@@ -43,19 +43,49 @@ main PROC
     ;   1. maxX, maxY = GetMaxXY
     ;   2. centerX = maxX / 2
     ;      centerY = maxY / 2
+    call GetMaxXY
+    dec  dl         ; highest column number = X-1
+    dec  dh         ; highest row number = Y-1
+	; Divide each register by 2 (shift right, or left?) to get
+	; the center coordinate
+    shr dl,1        ; x-coordinate of screen center
+    shr dh,1        ; y-coordinate of screen center
+    call Gotoxy     ; position the cursor at the center of the screen
     ;   move cursor to center of screen
     ;   3. cursor->x = centerX
     ;      cursor->y = centerY
-    ;   prompt user for two integers
-    ;   4. print("Enter an integer")
-    ;   5. integer1 = input()
-    ;   6. print("Enter another integer")
-    ;   7. integer2 = input()
-    ;   add the two integers
-    ;   8. sum = integer1 + integer2
-    ;   display the sum of the two integers
-    ;   9. print(f"The sum of {integer1} and {integer2} is {sum}")
 
+    ; Prompt the user to enter an integer
+    mov edx,OFFSET prompt
+    call WriteString
+    call ReadInt
+    mov integer1,eax        ; save the integer into integer1
+
+    ; Prompt the uesr to enter another integer
+	call Gotoxy
+    call WriteString
+    call ReadInt
+    mov integer2,eax        ; save the integer into integer2
+
+    ; add the two integers
+    mov eax,0
+    add eax,integer1
+    add eax,integer2
+    
+    ; Display the sum of the two integers
+	call Gotoxy
+    mov edx,OFFSET results
+    call WriteString
+    call WriteInt
+
+    
+    ; Display "Press any key to continue..." so that
+    ; we can see what the program did before it
+    ; closes
+	call Crlf
     call WaitMsg
+    call Clrscr
+
+    exit
 main ENDP
 END main
