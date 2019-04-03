ab23f0b412a91daa74e1477b276b4bcb70f4eaf4
Jay Ess <mentoce@gmail.com>
2019-04-03 13:45:57 -0700

    Basically working, formatting is off, and use push and pop for loop counters

--- assignments/assignment04/SimpleAddition2.asm
+++ assignments/assignment04/SimpleAddition2.asm
@@ -1,7 +1,7 @@
 ; Joshua Saunders
 ; CO SCI 917
 ; Assignment 4:
-; SimpleAddition1.asm
+; SimpleAddition2.asm
 ; 
 ; Prompt:
 ; 
@@ -18,23 +18,23 @@ INCLUDE Irvine32.inc
 
 ; Data segment
 .data
-integer1 DWORD ?    ; user input 1
+sum             DWORD 0 ; holds the sum of user-inputted ints
+intloopnum      DWORD 2 ; number of times to loop getting user input
+mainloopnum     DWORD 3 ; number of times to loop the program
 
 ; Cursor- and screen-related variables
-rows    BYTE ?      ; number of rows of screen
-cols    BYTE ?      ; number of columns of screen
+rows            BYTE ? ; number of rows of screen
+cols            BYTE ? ; number of columns of screen
 
 ; Printing-related variables
-prompt  BYTE "Enter an integer: ",0
-results BYTE "The sum of the two numbers is ",0
+prompt          BYTE "Enter an integer: ",0
+results         BYTE "The sum of the two numbers is ",0
 
 ; Code segment
 .code
 main PROC
-    ; How many times should we loop? 3 times.
-    mov ecx,3
-
-PRINTLOOP:
+    mov ecx,mainloopnum
+MAINLOOP:
     ; clear the screen
     call Clrscr
 
@@ -43,36 +43,37 @@ PRINTLOOP:
 	call GetScreenCenter		; (DH, DL) = (x-coord, y-coord)
 	mov rows,dh
 	mov cols,dl
+    ; Push the ECX (counter) to the stack so that we
+    ; can keep track of how many times the main loop
+    ; has run
+    push ecx
+    mov ecx,intloopnum
+
+    ; This loop handles getting input from the
+    ; user. This occurs intloopnum times
+GETINTLOOP:
 	call Gotoxy
     ; Prompt the user to enter an integer
     mov edx,OFFSET prompt
-    call WriteString
-    call ReadInt
-    mov integer1,eax
+    call PromptAndGetInt
+    add sum,eax
 
 	inc rows                	; go to the next row
-	mov dh,rows
-	mov dl,cols
-	call Gotoxy
-    ; Prompt the user to enter an integer
-    mov edx,OFFSET prompt
-    call WriteString
-    call ReadInt
-    
-    ; Add the two numbers. EAX is currently holding one of the
-    ; user's numbers so we only need to add the first to EAX
-    ; mov eax,integer1
-    add eax,integer1
-    ; Display the results
-    inc rows
-	mov dh,rows
+    loop GETINTLOOP
+
+    ; pop ECX so that we can keep track of how many
+    ; times the outer loop has run
+    pop ecx
+
 	mov dl,cols
     call Gotoxy
     mov edx,OFFSET results
     call WriteString
     call WriteInt
+    call Crlf
+    call WaitMsg
 
-	loop PRINTLOOP
+	loop MAINLOOP
 
     ; Display "Press any key to continue..." so that
     ; we can see what the program did before it
@@ -98,12 +99,12 @@ main ENDP
 ; Returns: (DH, DL) = (x-coord, y-coord)
 ;---------------------------------------------------------
 GetScreenCenter PROC uses eax
-	mov eax,0			; clear EAX & EDX
-	mov edx,0
+	; mov eax,0			; clear EAX & EDX
+	; mov edx,0
 
 	; find the size of the screen buffer
 	; - AX: number of rows
-	; - DL: number of columns
+	; - DX: number of columns
 	CALL GetMaxXY
 	shr al,1			; divide by 2 to get center
 	shr dl,1
@@ -111,4 +112,19 @@ GetScreenCenter PROC uses eax
 
 	ret					; cooridnates are in EDX
 GetScreenCenter ENDP
+
+;---------------------------------------------------------
+; PromptAndGetInt
+; Prompts the user for input and returns the input
+; Receives: None
+; Returns: (DH, DL) = (x-coord, y-coord)
+;---------------------------------------------------------
+PromptAndGetInt PROC
+    ; The prompt for the string needs to be
+    ; in EDX before calling this procedure
+    call WriteString
+    call ReadInt
+
+    ret
+PromptAndGetInt ENDP
 END main
