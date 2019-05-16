789b9ff0b58e62805e343253bdb06ec156b24f39
Jay Ess <mentoce@gmail.com>
2019-05-16 10:38:55 -0700

    Finish assignment

--- assignments/assignment06/GreatestCommonDivisor.asm
+++ assignments/assignment06/GreatestCommonDivisor.asm
@@ -35,17 +35,44 @@ INCLUDE Irvine32.inc
 
 ; Data segment
 .data
-XOR_BIT_MASK DWORD  0FFFFFFFFh
+ABS_VAL_BIT_MASK DWORD  0FFFFFFFFh
+
+; To hold the numerical values entered by the user
+intX    DWORD ?
+intY    DWORD ?
+
+; Prompts and output
+integerPrompt BYTE "Please enter an integer: ",0
+results1      BYTE "The greatest common divisor of ",0
+results2      BYTE " and ",0
+results3      BYTE " is ",0
 
 ; Code segment
 .code
 main PROC
-    mov eax,5
-    mov ebx,3
-    ; call GCD
-    call Modulo
-    mov eax,ebx
-    call WriteInt
+    mov edx,OFFSET integerPrompt
+    ; Get the two integers from the user
+    ; First integer
+    call WriteString
+    call ReadInt
+    mov intX,eax
+
+    ; Second integer
+    call WriteString
+    call ReadInt
+    mov intY,eax
+
+	; Recover the inputs
+    mov eax,intX
+    mov edx,intY
+    call GCD
+
+	call Crlf
+
+    mov ebx,eax
+	mov eax,intX
+    mov edx,intY
+    call PrintResults
 	exit
 main ENDP
 
@@ -57,26 +84,95 @@ AbsVal PROC
 ;---------------------------------------------------------
     ; 1. Add -1 to the number
     ; 2. Form one's complement
-    add eax,-1
-    xor eax,XOR_BIT_MASK
+	.IF (eax < 0)
+		add eax,-1
+		xor eax,ABS_VAL_BIT_MASK
+	.ENDIF
 	ret
 AbsVal ENDP
 
 
 ;---------------------------------------------------------
-Modulo PROC uses ECX
+Modulo PROC
 ; Calculates the modulo of two numbers according to the
 ;   the formula n = x % y
 ; Receives: EAX = the first number (x)
 ;           EDX = the second number (y)
 ; Returns: EBX = the modulo (n) of x and y
 ;---------------------------------------------------------
-    mov ecx,edx
+	; eax & edx get overwritten during division, so push
+	; them to the stack
+    push edx
+	push eax
+	mov ebx,edx
 	mov edx,0
-	push eax	; eax gets overwritten during division, so copy it
     div ebx
+	; div stores the remainder in edx, so move it to ebx
     mov ebx,edx
+	; get eax and edx back
 	pop eax
+    pop edx
     ret
 Modulo ENDP
+
+;---------------------------------------------------------------
+GCD PROC uses EBX
+; Calculates the greatest common divisor (GCD) of two integers
+; Receives: EAX = the first number (x)
+;           EDX = the second number (y)
+; Returns: EAX = greatest common divisor of the two numbers
+;---------------------------------------------------------------
+    ; 1. Find the absolute value of each number
+    ; 2. Find the remainder (n) of x and y
+    ; 3. Set x = y, y = n
+    ; 4. Goto 2 while y > 0
+    .IF (eax != 0 && edx != 0)
+        ; Step 1
+        call AbsVal ; x = abs(x)
+        push eax    ; store eax so we can use it later
+        mov eax,edx
+        call AbsVal ; y = abs(y)
+        mov edx,eax
+        pop eax     ; get eax back
+        .REPEAT
+            ; Step 2
+            call Modulo ; n = x % y
+            mov eax,edx ; x = y
+            mov edx,ebx ; y = n
+        ; Step 4
+        .UNTIL (edx <= 0)
+    .ENDIF
+
+	ret
+GCD ENDP
+
+;---------------------------------------------------------
+PrintResults PROC
+; Prints the of the calculation of the GCD proc
+; Receives: EAX = the first number
+;           EDX = the second number
+;           EBX = the greatest common divisor
+;---------------------------------------------------------
+    push edx
+
+    ; The follwing code block prints:
+    ; The greatest common divisor of <intX> and <intY> is <GCD>
+    ; where
+    ; <intX> and <intY> are the integers enterd by the user
+    ; <GCD> is the greatest common divisor of <intX> and <intY>
+    mov edx,OFFSET results1
+    call WriteString
+    call WriteInt
+    mov edx,OFFSET results2
+	call WriteString
+    pop edx
+    mov eax,edx
+    call WriteInt
+    mov edx,OFFSET results3
+    call WriteString
+    mov eax,ebx
+    call WriteInt
+
+	ret
+PrintResults ENDP
 END main
